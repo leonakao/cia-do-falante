9f599ea23efd87bca0e97f96885c3ba5ce00bb64
Leonardo Nakao <leonardo.nakao@wisereducacao.com>
2025-04-26 18:11:30 -0300

    fix: remove HeroSection component from sections exports

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,4 +1,3 @@
 export { default as AboutSection } from './AboutSection';
 export { default as ContactSection } from './ContactSection';
-export { default as HeroSection } from './HeroSection';
 export { default as ServicesSection } from './ServicesSection';
